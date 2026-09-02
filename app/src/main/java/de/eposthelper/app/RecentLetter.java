2fc9b701fd67d394b7ffb7cb86ed7af0593540f8
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-09-02 17:42:53 +0200

    Add recent LetterXpress job model

---
+++ app/src/main/java/de/eposthelper/app/RecentLetter.java
@@ -0,0 +1,11 @@
+package de.eposthelper.app;
+
+public final class RecentLetter {
+    public String id="";
+    public String filename="";
+    public String status="";
+    public String address="";
+    public String createdAt="";
+    public double amount=0;
+    public double vat=0;
+}
